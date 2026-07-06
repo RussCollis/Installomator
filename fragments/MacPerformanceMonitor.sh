80fb6248e4445020b7b4a18fdcef4f0eaf1d4568
RussCollis <russell.collis@kpmg.co.uk>
2026-07-06 12:20:58 +0100

    Create MacPerformanceMonitor.sh

---
+++ fragments/MacPerformanceMonitor.sh
@@ -0,0 +1,7 @@
+    name="Mac Performance Monitor"
+    type="pkg"
+    packageID="com.zestyowl.mac-performance-monitor"
+    downloadURL=$(downloadURLFromGit Zesty0wl mac-performance-monitor "pkg")
+    appNewVersion=$(versionFromGit Zesty0wl mac-performance-monitor)
+    expectedTeamID=""
+    ;;
