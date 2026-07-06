c604ee6a5b3aaf1e4115d057ad02c330e6ee43ba
RussCollis <russell.collis@kpmg.co.uk>
2026-07-06 12:22:23 +0100

    Update MacPerformanceMonitor.sh

--- fragments/MacPerformanceMonitor.sh
+++ fragments/MacPerformanceMonitor.sh
@@ -3,5 +3,5 @@
     packageID="com.zestyowl.mac-performance-monitor"
     downloadURL=$(downloadURLFromGit Zesty0wl mac-performance-monitor "pkg")
     appNewVersion=$(versionFromGit Zesty0wl mac-performance-monitor)
-    expectedTeamID=""
+    expectedTeamID="8352865GK4"
     ;;
